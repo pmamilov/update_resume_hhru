3fc247d9a16849718440e0d778acddeac219aeb6
PM <pmamilov@ya.ru>
2022-03-25 17:27:03 +0300

    Add cycle FOR to simple code

--- hhru_new4res.py
+++ hhru_new4res.py
@@ -8,20 +8,20 @@ load_dotenv(dotenv_file)
 
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 REFRESH_TOKEN = os.getenv('REFRESH_TOKEN')
-RESUME_ID1 = os.getenv('RESUME_ID1')
-RESUME_ID2 = os.getenv('RESUME_ID2')
-RESUME_ID3 = os.getenv('RESUME_ID3')
-RESUME_ID4 = os.getenv('RESUME_ID4')
-
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
+ALL_RESUMES = []
+resid = 'RESUME_ID'
+get_num = 0
+upd_url = []
+
+while os.getenv(resid + str(get_num)):
+    ALL_RESUMES.append(os.getenv(resid + str(get_num)))
+    upd_url.append(f'https://api.hh.ru/resumes/{ALL_RESUMES[get_num]}/publish/')
+    get_num += 1
 
 headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
 body = {'grant_type': 'refresh_token', 'refresh_token': REFRESH_TOKEN}
-update_url1 = f'https://api.hh.ru/resumes/{RESUME_ID1}/publish/'
-update_url2 = f'https://api.hh.ru/resumes/{RESUME_ID2}/publish/'
-update_url3 = f'https://api.hh.ru/resumes/{RESUME_ID3}/publish/'
-update_url4 = f'https://api.hh.ru/resumes/{RESUME_ID4}/publish/'
 refresh_url = f'https://hh.ru/oauth/token/'
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN)
 
@@ -31,26 +31,17 @@ def send_message(message):
 
 
 def update_resume():
-    response1 = requests.post(update_url1, headers=headers)
-    response2 = requests.post(update_url2, headers=headers)
-    response3 = requests.post(update_url3, headers=headers)
-    response4 = requests.post(update_url4, headers=headers)
-    if response1.status_code == 204 and response2.status_code == 204 and response3.status_code == 204 and response4.status_code == 204:
-        return send_message('Резюме успешно обновлено!')
-    error_code1 = response1.status_code
-    error_code2 = response2.status_code
-    error_code3 = response3.status_code
-    error_code4 = response4.status_code
-    error_value1 = response1.json()['errors'][0]['value']
-    error_value2 = response2.json()['errors'][0]['value']
-    error_value3 = response3.json()['errors'][0]['value']
-    error_value4 = response4.json()['errors'][0]['value']
-    send_message(f'Ошибка {error_code1}: {error_value1}')
-    send_message(f'Ошибка {error_code2}: {error_value2}')
-    send_message(f'Ошибка {error_code3}: {error_value3}')
-    send_message(f'Ошибка {error_code4}: {error_value4}')
-    if error_value1 == 'token_expired':
-        refresh_token()
+    for i in range(get_num):
+        response_n.append(requests.post(upd_url[i], headers=headers))
+
+        if response_n[i].status_code == 204:
+            send_message('Резюме успешно обновлено!')
+        else:
+            error_code.append(response_n[i].status_code)
+            error_value.append(response_n[i].json()['errors'][0]['value'])
+            send_message(f'Ошибка {error_code[i]}: {error_value[i]}')
+            if error_value1 == 'token_expired':
+                refresh_token()
 
 
 def refresh_token():
